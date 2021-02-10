8d67501d5e8562eb45df396dd8e06336145e3f48
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-02-10 15:19:22 +0100

    add data stubs for testing

---
+++ stubs.js
@@ -0,0 +1,60 @@
+const stubs = [
+  {
+    "name": "Colombia",
+    "topLevelDomain": [".co"],
+    "alpha2Code": "CO",
+    "alpha3Code": "COL",
+    "callingCodes": ["57"],
+    "capital": "Bogotá",
+    "altSpellings": ["CO", "Republic of Colombia", "República de Colombia"],
+    "region": "Americas",
+    "subregion": "South America",
+    "population": 48759958,
+    "latlng": [4.0, -72.0],
+    "demonym": "Colombian",
+    "area": 1141748.0,
+    "gini": 55.9,
+    "timezones": ["UTC-05:00"],
+    "borders": ["BRA", "ECU", "PAN", "PER", "VEN"],
+    "nativeName": "Colombia",
+    "numericCode": "170",
+    "currencies": [{
+        "code": "COP",
+        "name": "Colombian peso",
+        "symbol": "$"
+    }],
+    "languages": [{
+        "iso639_1": "es",
+        "iso639_2": "spa",
+        "name": "Spanish",
+        "nativeName": "Español"
+    }],
+    "translations": {
+        "de": "Kolumbien",
+        "es": "Colombia",
+        "fr": "Colombie",
+        "ja": "コロンビア",
+        "it": "Colombia",
+        "br": "Colômbia",
+        "pt": "Colômbia"
+    },
+    "flag": "https://restcountries.eu/data/col.svg",
+    "regionalBlocs": [{
+        "acronym": "PA",
+        "name": "Pacific Alliance",
+        "otherAcronyms": [],
+        "otherNames": ["Alianza del Pacífico"]
+    }, {
+        "acronym": "USAN",
+        "name": "Union of South American Nations",
+        "otherAcronyms": ["UNASUR", "UNASUL", "UZAN"],
+        "otherNames": ["Unión de Naciones Suramericanas", "União de Nações Sul-Americanas", "Unie van Zuid-Amerikaanse Naties", "South American Union"]
+    }]
+  },
+  {"name":"Poland","topLevelDomain":[".pl"],"alpha2Code":"PL","alpha3Code":"POL","callingCodes":["48"],"capital":"Warsaw","altSpellings":["PL","Republic of Poland","Rzeczpospolita Polska"],"region":"Europe","subregion":"Eastern Europe","population":38437239,"latlng":[52.0,20.0],"demonym":"Polish","area":312679.0,"gini":34.1,"timezones":["UTC+01:00"],"borders":["BLR","CZE","DEU","LTU","RUS","SVK","UKR"],"nativeName":"Polska","numericCode":"616","currencies":[{"code":"PLN","name":"Polish złoty","symbol":"zł"}],"languages":[{"iso639_1":"pl","iso639_2":"pol","name":"Polish","nativeName":"język polski"}],"translations":{"de":"Polen","es":"Polonia","fr":"Pologne","ja":"ポーランド","it":"Polonia","br":"Polônia","pt":"Polónia","nl":"Polen","hr":"Poljska","fa":"لهستان"},"flag":"https://restcountries.eu/data/pol.svg","regionalBlocs":[{"acronym":"EU","name":"European Union","otherAcronyms":[],"otherNames":[]}],"cioc":"POL"},
+  {"name":"Germany","topLevelDomain":[".de"],"alpha2Code":"DE","alpha3Code":"DEU","callingCodes":["49"],"capital":"Berlin","altSpellings":["DE","Federal Republic of Germany","Bundesrepublik Deutschland"],"region":"Europe","subregion":"Western Europe","population":81770900,"latlng":[51.0,9.0],"demonym":"German","area":357114.0,"gini":28.3,"timezones":["UTC+01:00"],"borders":["AUT","BEL","CZE","DNK","FRA","LUX","NLD","POL","CHE"],"nativeName":"Deutschland","numericCode":"276","currencies":[{"code":"EUR","name":"Euro","symbol":"€"}],"languages":[{"iso639_1":"de","iso639_2":"deu","name":"German","nativeName":"Deutsch"}],"translations":{"de":"Deutschland","es":"Alemania","fr":"Allemagne","ja":"ドイツ","it":"Germania","br":"Alemanha","pt":"Alemanha","nl":"Duitsland","hr":"Njemačka","fa":"آلمان"},"flag":"https://restcountries.eu/data/deu.svg","regionalBlocs":[{"acronym":"EU","name":"European Union","otherAcronyms":[],"otherNames":[]}],"cioc":"GER"},  
+  {"name":"Japan","topLevelDomain":[".jp"],"alpha2Code":"JP","alpha3Code":"JPN","callingCodes":["81"],"capital":"Tokyo","altSpellings":["JP","Nippon","Nihon"],"region":"Asia","subregion":"Eastern Asia","population":126960000,"latlng":[36.0,138.0],"demonym":"Japanese","area":377930.0,"gini":38.1,"timezones":["UTC+09:00"],"borders":[],"nativeName":"日本","numericCode":"392","currencies":[{"code":"JPY","name":"Japanese yen","symbol":"¥"}],"languages":[{"iso639_1":"ja","iso639_2":"jpn","name":"Japanese","nativeName":"日本語 (にほんご)"}],"translations":{"de":"Japan","es":"Japón","fr":"Japon","ja":"日本","it":"Giappone","br":"Japão","pt":"Japão","nl":"Japan","hr":"Japan","fa":"ژاپن"},"flag":"https://restcountries.eu/data/jpn.svg","regionalBlocs":[],"cioc":"JPN"},
+];
+
+
+export default stubs;
