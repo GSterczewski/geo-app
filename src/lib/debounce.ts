3e90a579ac5e37838861845af417bcc94cb5be7d
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-02-14 16:02:07 +0100

    add debounce function

---
+++ src/lib/debounce.ts
@@ -0,0 +1,14 @@
+import { time } from "console";
+
+export default function debouce<F extends (...args: any)=> any >(callback: F, wait = 100 ){
+  
+  let timeout = 0;
+  return (...args: any[]) => {
+    const later = () => {
+      timeout = 0;
+      callback.apply(null, args)
+    }
+    clearTimeout(timeout);
+    timeout = window.setTimeout(later,wait);
+  }
+}
