d12b2e5825be3710860d80f7bcb6f3d6633a6762
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-02-16 11:40:42 +0100

    closes #6

--- src/components/RegionSelect.tsx
+++ src/components/RegionSelect.tsx
@@ -6,12 +6,14 @@ export default function RegionSelect(){
   const { regions, selectRegion } = useCountries();
   const  [selectedRegion, setRegion] = useState("Filter by region");
   
+  
   const handleChange = (region: string) => {
     setRegion(region);
     selectRegion(region as WorldRegion);
   };
+  
   return (
-    <select className="input region-select" value={selectedRegion} onChange={e => handleChange(e.target.value)}>
+    <select className="input region-select"  value={selectedRegion}  onChange={e => handleChange(e.target.value)}>
       <option disabled  defaultValue="">Filter by region</option>
       {Object.values(regions).map(region => <option key={region} value={region} >{region}</option>)}
     </select>
