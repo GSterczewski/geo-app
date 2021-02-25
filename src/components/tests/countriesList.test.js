caaa8e84fdf56b2d00e32506fa384763f1ec7214
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-02-25 10:45:11 +0100

    update tests

---
+++ src/components/tests/countriesList.test.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import CountriesList from "components/CountriesList";
+import { BrowserRouter as Router } from "react-router-dom";
+let container;
+
+beforeEach(()=>{
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(()=>{
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe("ContainerList", ()=>{
+  it("renders component and message, when empty array was passed as prop", ()=>{
+    act(()=>{
+      ReactDOM.render(<CountriesList countries={[]} />, container);
+    });
+    const heading = document.querySelector("h1");
+    expect(heading.textContent.length).toBeGreaterThan(0);
+  });
+  it("renders component and message, when no props were passed", ()=>{
+    act(()=>{
+      ReactDOM.render(<CountriesList />, container);
+    });
+    const heading = document.querySelector("h1");
+    expect(heading.textContent.length).toBeGreaterThan(0);
+  });
+  it("renders component and list of countries", ()=>{
+    const stubCountries = [{
+      name: "Poland",
+      capital: "Warsaw",
+      population: 37000000,
+      flag: "urlToFlag",
+      region: "Europe",
+      alpha3Code: "POL"
+
+    },{
+      name: "Germany",
+      capital: "Berlin",
+      population: 137000000,
+      flag: "urlToFlag",
+      region: "Europe",
+      alpha3Code: "GER"
+
+    }]
+    act(()=>{
+      ReactDOM.render(<Router><CountriesList countries={stubCountries} /> </Router>, container);
+    });
+    const ul = document.querySelector("ul");
+    const children = ul.children;
+    expect(ul).toBeDefined();
+    expect(children).toBeDefined();
+    expect(children.length).toEqual(2);
+  })
+})
